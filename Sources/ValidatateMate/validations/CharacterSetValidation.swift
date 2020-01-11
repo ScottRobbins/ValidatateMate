449e04f5f0cdfb3edf91ab1151f31646333ac645
Scott Robbins <scott.robbins@mobiledefense.com>
2020-01-11 02:01:16 -0500

    Add CharacterSet validation

---
+++ Sources/ValidatateMate/validations/CharacterSetValidation.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+public struct CharacterSetError: ValidationError {
+  public let failureDescription: String?
+  
+  init(failureDescription: String? = nil) {
+    self.failureDescription = failureDescription
+  }
+}
+
+public extension Validation where T: StringProtocol {
+  static func characterSet(_ characterSet: CharacterSet) -> Validation<T> {
+    Validation {
+      if let range = $0.rangeOfCharacter(from: characterSet.inverted) {
+        let message = "contains an invalid character: '\($0[range])'"
+        return .failure(.init(CharacterSetError(failureDescription: message)))
+      } else {
+        return .success("contains valid characters for character set: \(characterSet)")
+      }
+    }
+  }
+}
