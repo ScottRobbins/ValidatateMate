d3ae1539c992d22971dab0a72df85a9f3ae78806
Scott Robbins <scott.robbins@mobiledefense.com>
2020-01-11 03:21:09 -0500

    Add validation for being in list

---
+++ Sources/ValidatateMate/validations/InValidation.swift
@@ -0,0 +1,25 @@
+public struct InError: ValidationError {
+  public let failureDescription: String?
+  
+  init(failureDescription: String? = nil) {
+    self.failureDescription = failureDescription
+  }
+}
+
+public extension Validation where T: Equatable {
+  static func `in`(_ array: T...) -> Validation<T> {
+    .in(array)
+  }
+  
+  static func `in`(_ array: [T]) -> Validation<T> {
+    Validation {
+      let list = array.map { "\($0)" }.joined(separator: ",")
+      if array.contains($0) {
+        return .success("is in \(list)")
+      } else {
+        let message = "is not in \(list)"
+        return .failure(.init(InError(failureDescription: message)))
+      }
+    }
+  }
+}
